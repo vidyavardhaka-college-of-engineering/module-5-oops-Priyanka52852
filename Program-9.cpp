76e14a66720d263598a7c62cb4ab116801bce985
Priyanka M <priyankalucky52852@gmail.com>
2021-04-01 16:05:01 +0000

    done

--- Program-9.cpp
+++ Program-9.cpp
@@ -4,14 +4,15 @@
 //Input: 12 6
 //Output: 0
 
+
 #include <iostream>
 using namespace std;
 int main()
 {    
     int a, b, r;
     std::cout << "Enter two numbers: ";
-    std::cin >>  >>; //Read the variables
-    r = a   b; //find the suitable operator
-    std::cout << ; //print the result
+    std::cin >>a>>b; //Read the variables
+    r=a%b; //find the suitable operator
+    std::cout <<r; //print the result
     return 0;
 }
